860c536ef49d0b4e06408ee89d8f30e783950bf5
Julio Meraki <julio.meraki@example.com>
2025-09-05 17:43:40 -0500

    Corregir import de index.css en src/index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from '../App';
-import './index.css';
+import '../index.css';
 import './i18n'; // Import i18next configuration
 import { AppContextProvider } from './context/AppContext';
 
