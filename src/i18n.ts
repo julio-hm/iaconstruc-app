338bd2c2155a0179d6cf263fb94e2234a283bd9f
Julio Meraki <julio.meraki@example.com>
2025-09-05 14:40:49 -0500

    "fix(i18n):-load-translations-from-public-directory"

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,31 +1,23 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
-
-// Import your translations
-import en from './locales/en.json';
-import es from './locales/es.json';
-
-const resources = {
-  en: {
-    translation: en
-  },
-  es: {
-    translation: es
-  }
-};
+import HttpApi from 'i18next-http-backend';
 
 i18n
+  // load translation using http -> see /public/locales
+  .use(HttpApi)
   // Detect user language
   .use(LanguageDetector)
   // Pass the i18n instance to react-i18next.
   .use(initReactI18next)
   // Init i18next
   .init({
-    resources,
     fallbackLng: 'en', // use en if detected lng is not available
     interpolation: {
       escapeValue: false // react already safes from xss
+    },
+    backend: {
+      loadPath: '/locales/{{lng}}.json'
     }
   });
 
